5df08ea41aeb73117b259c27b956b35f67116ec0
kohlerpop1 <kohlerpop1@gmail.com>
2023-01-05 21:27:47 -0500

    Revert "Fixed an uncaught exception!"
    This reverts commit 4f83aed74b86ab55f5e84e23003409658f4c0309.

--- src/main/java/github/scarsz/discordsrv/listeners/DiscordAccountLinkListener.java
+++ src/main/java/github/scarsz/discordsrv/listeners/DiscordAccountLinkListener.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -25,7 +25,6 @@ package github.scarsz.discordsrv.listeners;
 import github.scarsz.discordsrv.Debug;
 import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.api.events.DiscordPrivateMessageReceivedEvent;
-import github.scarsz.discordsrv.objects.managers.AccountLinkManager;
 import github.scarsz.discordsrv.util.DiscordUtil;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
@@ -85,24 +84,21 @@ public class DiscordAccountLinkListener extends ListenerAdapter {
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         Member member = event.getMember();
         // add linked role and nickname back to people when they rejoin the server
-	    AccountLinkManager accountLinkManager = DiscordSRV.getPlugin().getAccountLinkManager();
-	    if (accountLinkManager != null) {
-		    UUID uuid = accountLinkManager.getUuid(event.getUser().getId());
-		    if (uuid != null) {
-			    Role roleToAdd = DiscordUtil.resolveRole(DiscordSRV.config().getString("MinecraftDiscordAccountLinkedRoleNameToAddUserTo"));
-			    if (roleToAdd == null || roleToAdd.getGuild().equals(member.getGuild())) {
-				    if (roleToAdd != null) DiscordUtil.addRoleToMember(member, roleToAdd);
-				    else DiscordSRV.debug(Debug.GROUP_SYNC, "Couldn't add user to null role");
-			    } else {
-				    DiscordSRV.debug(Debug.GROUP_SYNC, "Not adding role to member upon guild join due to the guild being different! ("+roleToAdd.getGuild()+" / "+member.getGuild()+")");
-			    }
-			
-			    if (DiscordSRV.config().getBoolean("NicknameSynchronizationEnabled")) {
-				    OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
-				    DiscordSRV.getPlugin().getNicknameUpdater().setNickname(member, player);
-			    }
-		    }
-	    }
+        UUID uuid = DiscordSRV.getPlugin().getAccountLinkManager().getUuid(event.getUser().getId());
+        if (uuid != null) {
+            Role roleToAdd = DiscordUtil.resolveRole(DiscordSRV.config().getString("MinecraftDiscordAccountLinkedRoleNameToAddUserTo"));
+            if (roleToAdd == null || roleToAdd.getGuild().equals(member.getGuild())) {
+                if (roleToAdd != null) DiscordUtil.addRoleToMember(member, roleToAdd);
+                else DiscordSRV.debug(Debug.GROUP_SYNC, "Couldn't add user to null role");
+            } else {
+                DiscordSRV.debug(Debug.GROUP_SYNC, "Not adding role to member upon guild join due to the guild being different! (" + roleToAdd.getGuild() + " / " + member.getGuild() + ")");
+            }
+
+            if (DiscordSRV.config().getBoolean("NicknameSynchronizationEnabled")) {
+                OfflinePlayer player = Bukkit.getOfflinePlayer(uuid);
+                DiscordSRV.getPlugin().getNicknameUpdater().setNickname(member, player);
+            }
+        }
     }
 
 }
